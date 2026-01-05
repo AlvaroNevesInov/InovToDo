974ee39706d88d3214110a78cd047820c15c271c
Álvaro Neves <alvaro.neves2019@gmail.com>
2026-01-05 17:43:03 +0000

    Desabilitar ForceHttps - Railway gerencia HTTPS via proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,14 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        // Trust all proxies for Railway/Docker environment
+        $middleware->trustProxies(at: '*', headers:
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR |
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST |
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT |
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO
+        );
+
         // Security headers middleware - adiciona headers de segurança
         $middleware->append(\App\Http\Middleware\SecurityHeaders::class);
 
